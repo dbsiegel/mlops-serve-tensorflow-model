e778771847f1f2af6de9d763fd28338afcf1f02e
Deborah Siegel <deborah.siegel@gmail.com>
2023-04-17 21:46:10 +0000

    setup shell scripts

--- serve.sh
+++ serve.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 tensorflow_model_server \
   --rest_api_port=8501 \
   --model_name=fashion_model \
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 echo "deb http://storage.googleapis.com/tensorflow-serving-apt stable tensorflow-model-server tensorflow-model-server-universal" | sudo tee /etc/apt/sources.list.d/tensorflow-serving.list && \
 curl https://storage.googleapis.com/tensorflow-serving-apt/tensorflow-serving.release.pub.gpg | sudo apt-key add -
 sudo apt update
